26ac8a5a4570d787b0355b6e8e112d546975d29d
AkitoDR <lara.muller@workmail.com>
2021-02-26 16:10:39 -0300

    deleted seeders

--- server/database/seeders/StudentsSeeder.ts
+++
@@ -1,9 +0,0 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
-import ImportStudents from '../../app/Utils/ImportStudents';
-import path from 'path';
-
-export default class StudentsSeederSeeder extends BaseSeeder {
-  public async run() {
-    //ImportStudents(path.join('files/Oficio nº 10_2020_Monitorias 2020-2.xlsx'));
-  }
-}
