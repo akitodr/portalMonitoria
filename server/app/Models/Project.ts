ade60bf5fa7e4d17d436d6d0739217411e5e3956
AkitoDR <lara.muller@workmail.com>
2021-02-26 16:11:05 -0300

    project table

--- server/app/Models/Project.ts
+++ server/app/Models/Project.ts
@@ -1,6 +1,8 @@
 import { DateTime } from 'luxon';
-import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm';
+import { BaseModel, column, HasMany, hasMany, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm';
 import Discipline from './Discipline';
+import Student from './Student';
+import Teacher from './Teacher';
 
 export default class Project extends BaseModel {
   @column({ isPrimary: true })
@@ -11,6 +13,12 @@ export default class Project extends BaseModel {
 
   @hasMany(() => Discipline)
   public disciplines: HasMany<typeof Discipline>;
+
+  @manyToMany(() => Student)
+  public students: ManyToMany<typeof Student>;
+
+  @manyToMany(() => Teacher)
+  public teachers: ManyToMany<typeof Teacher>;
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime;
