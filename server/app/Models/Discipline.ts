6cccf886ed5fb8ebad2fb4f70096218ff84b950c
AkitoDR <lara.muller@workmail.com>
2021-02-26 16:12:29 -0300

    discipline model

--- server/app/Models/Discipline.ts
+++ server/app/Models/Discipline.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon';
-import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm';
+import { BaseModel, BelongsTo, belongsTo, column, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm';
 import Project from './Project';
+import ProjectStudent from './ProjectStudent';
 
 export default class Discipline extends BaseModel {
   @column({ isPrimary: true })
@@ -15,8 +16,14 @@ export default class Discipline extends BaseModel {
   @column()
   public hours: string;
 
+  @column()
+  public projectId: number;
+
   @belongsTo(() => Project)
   public project: BelongsTo<typeof Project>;
+
+  @manyToMany(() => ProjectStudent)
+  public projectStudents: ManyToMany<typeof ProjectStudent>;
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime;
